a3c9f2b57d6574150c30934eb44f065384fe566f
Zach Gershkoff <zgershkoff@users.noreply.github.com>
2020-12-12 16:20:21 -0600

    snapshot of cleaning up

--- src/gaussiannb.py
+++ src/gaussiannb.py
@@ -41,8 +41,8 @@ def preprocess(datafile = infile):
 
     # remove certain attributes from data
     # without this, the model will work with 100% accuracy
-    # allowed_attribs = [2, 4] + list(range(6, 14))
-    allowed_attribs = [0, 3, 5]
+    allowed_attribs = [0, 1, 2, 4] + list(range(6, 14))
+    # here 3 and 5 are removed, meaning K_5 and K_3,3 duals
     data = [[line[i] for i in allowed_attribs] for line in data]
 
     ### test_size is the percentage of events assigned to the test set
@@ -50,17 +50,8 @@ def preprocess(datafile = infile):
     matroids_train, matroids_test, labels_train, labels_test = train_test_split(
         data, is_graphic, test_size=0.2, random_state=42)
 
+    print("Example event:")
     print(matroids_test[0])
-    print("how many ambiguous?")
-    print(sum(1 for line in matroids_test if not (line[0] or line[1] or line[2])))
-    print(len([i for i in range(len(matroids_test)) if sum(matroids_test[i]) == 0]))
-
-
-    # # let it discard a redundant feature or two
-    # selector = SelectPercentile(f_classif, percentile=90)
-    # selector.fit(matroids_train, labels_train)
-    # matroids_train_transformed = selector.transform(matroids_train)
-    # matroids_test_transformed  = selector.transform(matroids_test)
 
     ### info on the data
     print("Number of graphic matroids in training data:", sum(labels_train))
@@ -69,30 +60,39 @@ def preprocess(datafile = infile):
     print("Number of nongraphic matroids in test data:", len(labels_test)-sum(labels_test))
 
     return matroids_train, matroids_test, labels_train, labels_test
-    # return matroids_train_transformed, matroids_test_transformed, labels_train, labels_test
 
 def predict_if_graphic(matroids_train, matroids_test, labels_train, labels_test):
     clf = GaussianNB()
     clf.fit(matroids_train, labels_train)
     accuracy = clf.score(matroids_test, labels_test)
-    print(len(matroids_test))
-    print(len(matroids_test[0]))
+
+    # First row is nongraphic matroids
+    # First column is matroids predicted to be nongraphic
     print(confusion_matrix(labels_test, clf.predict(matroids_test)))
-    print()
+
+    # With 0 and 1 removed, the important parameters 3 and 5 have shifted
+    # to indices 1 and 3
+    print("number of dumb positives")
+    print(len([i for i in range(len(matroids_test)) if \
+     not matroids_test[i][1] and not matroids_test[i][3] \
+     and clf.predict(matroids_test)[i]]))
     print(accuracy)
 
-    print("number of dumb false positives")
-    print(matroids_test[0])
-    print(len([i for i in range(len(matroids_test)) if sum(matroids_test[i]) == 0 and clf.predict(matroids_test)[i]]))
-    return accuracy
-
 def simple_statistics(datafile = "../json/hr-sz13-rk08-results.json"):
+    """
+    returns simple statistics about the matroid
+
+    INPUT: A file path
+    OUTPUT: None. Prints statistics.
+    """
     with open(datafile) as f:
         data_strings = json.load(f)
     data = [unwrap_line(line) for line in data_strings]
 
     print("ratio connected:", (sum(data[9]) / len(data)))
-    print(sum(1 for line in data if (line[1] and not (line[0] or line[3] or line[5]))))
+    print("Number of gsraphic matroids:",
+        sum(1 for line in data if (line[1] \
+            and not (line[0] or line[3] or line[5]))))
 
 
 if __name__ == "__main__":
--- src/process.py
+++ src/process.py
@@ -6,32 +6,94 @@ import sage.matroids.matroids_catalog as matroids
 from sage.graphs.graph_generators import graphs
 import json
 
-def string_to_matrix(repstring, rank):
+def string_to_matrix(repstring, rank = 0):
     """
-    INPUT: string as from Gordon Royle's binary matroid data
+    interprets a string of space-separated integers as a binary matrix
+
+    INPUT:  string as from Gordon Royle's binary matroid data, and the
+            rank of the matrioid.
     OUTPUT: binary matrix
+
+    The input strings are the number of elements followed by decimal
+    representations of the binary vectors. For example, the string
+    "4 1 2 4 7 " is a rank-3 4-element matroid given by
+    [[0,0,1], [0,1,0], [1,0,0], [1,1,1]].
+    Although the rank is given as input because the data are organized
+    by rank, it can be surmised from the number of bits needed for the
+    last number.
     """
-    elts = repstring.strip().split(" ")[1:]
+    elts = [int(n) for n in repstring.strip().split(" ")[1:]]
     mat = []
+    if not rank:
+        rank = elts[-1].bit_length()
     for n in elts:
-        # Todo: adjust according to rank
-        mat.append([int(c) for c in format(int(n), '0' + str(rank) + 'b')])
+        mat.append([int(c) for c in format(n, '0' + str(rank) + 'b')])
         # alternate solution: bin(int(n))[2:].zfill(7)
     return Matrix(GF(2), mat).transpose()
 
 def matrix_to_matroid(mat):
+    """
+    returns the matroid of a given matrix
+
+    INPUT:   A matrix.
+    OUPTUT:  A matroid.
+    """
     return Matroid(mat)
 
-# these are stubs
-# improve speed and add checks for matroids with rank or corank 0
-def girth(M):
-    return min(len(C) for C in M.circuits())
+# These next two methods could be considered stubs.
+# Enumerating circuits and cocircuits is computationally hard, and
+# there's not a clean way to start with the smallest ones.
+# A different approach could be a binary search on the possible ranks,
+# but this isn't the right thing to focus on right now.
+# And besides, it's NP-hard (A. Vardy, The intractability of computing
+# the minimum distance of a code)
+def girth(M, circuits):
+    """
+    returns the size of the smallest circuit
 
-def cogirth(M):
-    return min(len(C) for C in M.cocircuits())
+    INPUT:   A matroid and its set of circuits
+    OUTPUT:  Integer
+    """
+    return min(len(C) for C in circuits)
+
+def cogirth(M, circuits):
+    """
+    returns the size of the smallest cocircuit
+
+    INPUT:   A matroid and its set of circuits
+    OUTPUT:  Integer
+    """
+    if len(M.coloops()) > 0:
+        return 1
+    elif not M.is_cosimple():
+        return 2
+    return min(len(C) for C in circuits)
+
+def has_triangle(M, circuits):
+    """
+    returns true if M has a triangle
+    INPUT:   A matroid and its set of circuits
+    OUTPUT:  Integer
+    """
+    return any(len(C) == 3 for C in circuits)
 
 def analyze_matroid(M):
-    attribs = [False] * 12 # 12 boolean variables
+    """
+    checks parameters of a matroid
+
+    INPUT:   A matroid
+    OUTPUT:  A list consisting of booleans and integers
+
+    The attributes measured are:
+    - Fano minor and dual
+    - K_{3,3} minor and dual
+    - K_5 minor and dual
+    - Triangle minor
+    - Cosimple
+    - 2, 3, and 4-connected
+    - Girth and cogirth
+    """
+    attribs = [False] * 11 # 11 boolean variables
     attribs.extend([0, 0]) # girth and cogirth
 
     # checking for minors can be improved with a database approach
@@ -42,22 +104,26 @@ def analyze_matroid(M):
     attribs[4] = M.has_minor(matroids.CompleteGraphic(5))
     attribs[5] = M.has_minor(matroids.CompleteGraphic(5).dual())
 
-    attribs[6] = M.has_line_minor(3)
-    attribs[7] = M.is_simple()
-    attribs[8] = M.is_cosimple()
-    attribs[9] = M.is_connected()
-    attribs[10] = M.is_3connected()
-    attribs[11] = M.is_4connected()
+    circuits = M.circuits() # this is the slow step
+    attribs[6] = has_triangle(M, circuits)
+    attribs[7] = M.is_cosimple()
+    attribs[8] = M.is_connected()
+    attribs[9] = M.is_3connected()
+    attribs[10] = M.is_4connected()
 
-    attribs[12] = girth(M)
-    attribs[13] = cogirth(M)
+    attribs[11] = girth(M, circuits)
+    attribs[12] = cogirth(M, circuits)
 
     return attribs
 
 def main():
-    InFile = "../data/smallBinaryMatroids/hr-sz13-rk05"
-    OutFile = "../json/hr-sz13-rk05-results.json"
-    rank = 5
+    # The problem with storing this in json is that I need to convert
+    # data to and from strings. Python's pickle may have been a better
+    # option for serialization, but I like having the option of moving
+    # this data to a different framework.
+    InFile = "../data/smallBinaryMatroids/hr-sz13-rk08"
+    OutFile = "../json/hr-sz13-rk08-results3.json"
+    rank = 8
 
     result = []
 
@@ -66,10 +132,10 @@ def main():
             mat = string_to_matrix(repstring, rank)
             M = matrix_to_matroid(mat)
             attribs = analyze_matroid(M)
-            result.append([repstring] + [str(a) for a in attribs])
+            result.append((repstring, attribs))
 
     with open(OutFile, 'w') as f:
-        f.write(json.dumps(result))
+        json.dump(result, f, indent=2)
 
 if __name__ == "__main__":
     main()
